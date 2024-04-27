a2d33961e19484b1e68a8de296a14fc39a5f5f26
jorgef <jorgeartur.f@gmail.com>
2024-04-26 23:54:08 -0300

    inicializacao do db e processo de migrates

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_migrate import Migrate
+from database import db
 
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py')
 
+#db
+db.init_app(app)
+from users import User
+from alunos import Aluno
+migrate = Migrate(app, db)
+
+#rotas
 from views import *
 
 if __name__ == '__main__':
